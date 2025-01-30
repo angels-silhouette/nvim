a1ef281073a191b582ae244390d63921800c7358
Lachlan <lachlan_ikeguchi@proton.me>
2025-01-30 19:24:28 +1000

    Fix: changed <C-m> for manual to <leader>m

--- lua/angels_silhouette/remap.lua
+++ lua/angels_silhouette/remap.lua
@@ -89,7 +89,7 @@ vim.keymap.set("n", "<leader>w", vim.cmd.w)
 -- telescope
 local telescope_builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
-vim.keymap.set('n', '<C-m>', telescope_builtin.man_pages, {})
+vim.keymap.set('n', '<leader>m', telescope_builtin.man_pages, {})
 
 -- git
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
